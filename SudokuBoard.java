ab3741b3e5fd4e639568257d3c98c7747e955f67
PrinceA00 <146418108+PrinceA00@users.noreply.github.com>
2025-04-16 12:16:45 -0700

    completed part one field, constructor and toString

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,4 +1,11 @@
 public class SudokuBoard{
-
+   private char[]][] board;// this create a field
+   
+   public SudokuBoard(String filename)/* This is our constructor/ Notice no use of  void because it is a constructor*/ {
+   
+   } 
+   public String toStrin(){
+      return "";
+   }
 
 }
