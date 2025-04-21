a4169ea41fa80221dd63457a6aa44f7ccac3fe5f
PrinceA00 <146418108+PrinceA00@users.noreply.github.com>
2025-04-20 22:00:57 -0700

    Add files via upload

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,11 +1,27 @@
-public class SudokuBoard{
-   private char[]][] board;// this create a field
-   
-   public SudokuBoard(String filename)/* This is our constructor/ Notice no use of  void because it is a constructor*/ {
-   
-   } 
-   public String toStrin(){
-      return "";
-   }
+import java.io.*;
+import java.util.Scanner;
 
+public class SudokuBoard {
+    private char[][] board;
+
+    public SudokuBoard(String filename) {
+        board = new char[9][9];
+        try {
+            Scanner fileScanner = new Scanner(new File(filename));
+            for (int row = 0; row < 9; row++) {
+                String line = fileScanner.nextLine().trim();
+                for (int col = 0; col < 9; col++) {
+                    board[row][col] = line.charAt(col);
+                }
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + filename);
+        }
+    }
+
+    public String toString() {
+        // To be completed next
+        return "";
+    }
 }
